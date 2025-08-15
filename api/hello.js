bab1ddeae15819496c10eb7289e9d36e06661a6a
Austin He <austinhe@Mac.gateway>
2025-08-16 09:04:44 +1000

    fix runtime error

--- api/hello.js
+++ api/hello.js
@@ -1,22 +1,19 @@
-// // export const config = {
-// //   runtime: 'edge',
-// // };
+export const config = {
+  runtime: 'edge',
+};
 
-// export default async function handler(req) {
-//   try {
-//     const ipInfo = await fetch('https://ipapi.co/json').then(r => r.json());
-//     const city = ipInfo.city || 'somewhere on Earth';
-//     return new Response(
-//       JSON.stringify({ message: `Hello from ${city}!` }),
-//       { headers: { 'Content-Type': 'application/json' } }
-//     );
-//   } catch (e) {
-//     return new Response(
-//       JSON.stringify({ message: 'Hello from the Edge!' }),
-//       { headers: { 'Content-Type': 'application/json' } }
-//     );
-//   }
-// }
 export default async function handler(req) {
-  return new Response("Hello from Edge!", { status: 200 });
+  try {
+    const ipInfo = await fetch('https://ipapi.co/json').then(r => r.json());
+    const city = ipInfo.city || 'somewhere on Earth';
+    return new Response(
+      JSON.stringify({ message: `Hello from ${city}!` }),
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  } catch (e) {
+    return new Response(
+      JSON.stringify({ message: 'Hello from the Edge!' }),
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  }
 }
